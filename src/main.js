92b721e61a14930caa1e587de593f8b6a87c978b
nathanyu8th <nathanyu8th@gmail.com>
2025-01-24 14:26:36 -0800

    documentation

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+/**
+ * Nathan Yu
+ * Rocket Patrol Refactor Remake
+ * 3 hours
+ * 1PT: Tracking High Score
+ * 1PT: Allowing player to control Rocket after fired
+ * 3PT: Display time remaining
+ * 5PT: New enemy Spaceship type
+ * 5PT: New timing/scoring mechanism based on hit/miss
+ * 5PT: Added particle emitter
+ * 
+ */
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
